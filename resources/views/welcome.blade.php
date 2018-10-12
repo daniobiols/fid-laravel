218ae5550f16804911d64fd94dd576a2df6886b7
Digital House <=>
2018-10-12 20:59:59 -0300

    welcome to fid

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    FID
                 </div>
 
                 <div class="links">
