47442cd6c1334795ff8f98cd80c08f944d81097f
daniobiols <danielaobiols@gmail.com>
2018-11-07 17:25:22 -0300

    paginacion product/index

--- resources/views/admin/Products/index.blade.php
+++ resources/views/admin/Products/index.blade.php
@@ -5,12 +5,40 @@
 @section('main')
 
 
-<ul>
-	@foreach ($products as $element)
-		<li>{{$element->name}}</li>
-		<li>{{$element->description}}</li>
-		<li>{{$element->product_code}}</li>
-	@endforeach
-</ul>
+<table class="table table-bordered table-striped">
+		<thead>
+			<tr>
+				<th>Nombre</th>
+				<th>Marca</th>
+				<th>Codigo</th>
+				<th>Precio</th>
+				<th>Accion</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($products as $product)
+				<tr>
+					<td>{{ $product->name }}</td>
+					<td>{{ $product->description }}</td>
+					<td>{{ $product->product_code }}</td>
+					<td>{{ $product->price }}</td>
+					<td>
+						<a class="btn btn-primary" href="/admin/products/{{$product->id}}/edit">
+							<span class="fa fa-pencil"></span>
+						</a>
+						<form action="/admin/products/{{$product->id}}" method="post">
+							@method('DELETE')
+							@csrf
+							<button class="btn btn-danger">
+								<span class="fa fa-trash"></span>
+							</button>
+						</form>
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+
+	{{ $products->links() }}
 
 @endsection
--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -6,17 +6,12 @@
  <div>
  	<ul>
  		<li>
-			<a href="products/index">Index Productos</a>
+			<a href="admin/products/index">Index Productos</a>
  		</li>
  		<li>
-			<a href="products/show">Show Productos</a>
+			<a href="admin/products/show">Show Productos</a>
  		</li>
  	</ul>
-{{-- 	<a href="products/index">Index Productos</a>
-	<a href="products/show">Show Productos</a>
-	<a href="/home">Agregar Producto</a>
-	<a href="/home">Editar Productos</a>
-	<a href="/home">Eliminar Productos</a> --}}
  		
  </div>
 @endsection
