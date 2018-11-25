83e2c5f9fa12a6d54fc4b0dbe0d4ae03e4c34c4b
alchemist43837 <freddy_castaneda@hotmail.com>
2018-11-25 17:04:38 -0300

    avance seeder Product

--- app/Purcharse.php
+++ app/Purcharse.php
@@ -8,8 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Purcharse extends Model
 {
     use SoftDeletes;
-    protected $fillable =
-          ['user_id','transaction_date','charge'];
+    protected $fillable =['user_id','transaction_date','charge'];
     protected $dates = ['deleted_at'];
 
     public function purchases_detail()
--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
 class ProductsSeeder extends Seeder
 {
     /**
@@ -11,10 +11,23 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
-      \DB::table('products')->insert([
-       ['name' => 'Indumentaria'],
-       ['name' => 'Calzado'],
-       ['name' => 'Accesorios']
-     ]);
+      $faker=Faker::create();
+      for ($i=0; $i <30; $i++) {
+        \DB::table('products')->insert((
+         'name' => $faker->name,
+         'trademark'=> $faker->company,
+         'category_id'=>rand(1,3),
+         'type_id'=>rand(1,3),
+         'subcategory_id'=>rand(1,22),
+         'description'=> $faker->text(50),
+         'price',
+         'price_list',
+         'product_code',
+         'size',
+         'quantity',
+         'color'
+       ));
+      }
+
     }
 }
